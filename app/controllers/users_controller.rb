87cac7ffa2c6d04a2bc24cb66cc0a2407d96f751
SLACKDROID <info.slacklife+git@gmail.com>
2017-11-18 05:55:05 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
     @user = User.find(params[:id])
   end
 
   def new
-    @User = User.new
+    @user = User.new
   end
 
   def create
-   @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
@@ -18,11 +20,11 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
+  
